34f86ddcb97c60653b792b4461be2b813d41b8bb
Davis <davisochieng4@gmail.com>
2020-03-08 04:08:35 -0700

    Object mutation

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,11 +1,26 @@
 import React from 'react'
 
-const Person = ({name, age})=>{
-    // console.log(props)
+const Person = ({id, name, age, deletePerson, change})=>{
+	const handleDel = ()=>{
+		deletePerson(id)
+	}
+	const changeHandler = (e)=>{
+		const name = e.target.value
+		return name
+	}
+	const handleChangeName = ()=>{
+		const name = this.changeHandler()
+		change(name)
+	}
+	
+	
     return (
         <div>
             <h3>{name}</h3>
             <p>{age}</p>
+			<input type='text' placeholder='Change Name' onChange={changeHandler}/>
+			<button onClick={()=>handleChangeName()}>Change Name</button>
+			<button onClick={()=>handleDel()}>Delete</button>
 
         </div>
     )
